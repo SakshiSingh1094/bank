56c6e81b8ffe932f2df9fe0b45ecb3ed8d56181e
Sakshi Singh <sakshisingh1094@gmail.com>
2025-02-25 09:27:00 +0530

    docker changes in accounts

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,12 +1,17 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message="Name cannot be null or empty")
+    @Size(min=5, max=30, message="length of customer name should be 5 to 30")
     private String name;
 
+    @NotEmpty(message="Email cannot be null or empty")
     private String email;
 
     private String mobileNumber;
